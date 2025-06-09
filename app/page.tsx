90b5c56c61a55ce3b025e23267982febbf88572f
Frenmy Garcia <frenmymanuel@gmail.com>
2025-06-09 11:24:16 +0200

    Actualización de diseño y componentes de Brillo Hogar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Header from "@/components/Header";
 import HowWeWork from "@/components/HowWeWork";
 import ProductosTools from "@/components/ProductosTools";
 import Services from "@/components/Services";
-import Image from "next/image";
 
 export default function Home() {
   return (
